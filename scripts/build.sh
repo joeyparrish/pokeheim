86062b47a071d1c771c2552106bff033cb790835
Joey Parrish <joey.parrish@gmail.com>
2022-06-14 11:40:52 -0700

    Add check for RELEASE_VERSION in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,4 +14,12 @@ fi
 
 cd "$(dirname "$0")"/..
 
+if [ "$BUILD_TYPE" == "Release" ]; then
+  if [ -z "$RELEASE_VERSION" ]; then
+    echo "You must set the environment variable \$RELEASE_VERSION," 1>&2
+    echo "and use semantic versioning." 1>&2
+    exit 1
+  fi
+fi
+
 msbuild Pokeheim.sln /p:Configuration="$BUILD_TYPE"
