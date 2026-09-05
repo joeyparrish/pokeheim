d8544ad710cf07d929fed2dc7b6dca810e5f43b4
Joey Parrish <joey.parrish@gmail.com>
2026-09-04 18:53:56 -0700

    Add mod launch script

---
+++ scripts/launch-mod.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Written for me (Joey), and working on Ubuntu 20.04 LTS.  I make no promises
+# that it will work for you.  See .github/workflows/ for repeatable
+# instructions to install necessary .NET tools.
+
+set -e
+
+cd ~/.local/share/Steam/steamapps/common/Valheim
+./start_game_bepinex.sh \
+    ~/.local/share/Steam/steamapps/common/Valheim/valheim.x86_64 \
+    -force-glcore -console
